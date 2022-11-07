b54056e7883e4d5b994c2f955c20e3d8c50f3cb2
Shad0wz93 <Shadowz9@icloud.com>
2022-11-07 11:59:10 +0100

    5

--- partie4.sql
+++ partie4.sql
@@ -1,1 +1,4 @@
-SELECT * FROM commentaire WHERE id_film = 43;
+SELECT * FROM commentaire
+INNER JOIN film ON commentaire.id_film = film.id 
+INNER JOIN user ON commentaire.id_user = user.id
+WHERE id_film = 43;
